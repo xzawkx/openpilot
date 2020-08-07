297641590ac77c0bee4961e8efa448d8e342ab63
Martin Lillepuu <martin@mlp.ee>
2020-08-07 18:03:21 +0300

    Fix waifu paths

--- selfdrive/ui/themes/waifu/fonts.hpp
+++ selfdrive/ui/themes/waifu/fonts.hpp
@@ -1,3 +1,3 @@
-#define FONT_SANS_REGULAR "themes/base/fonts/cheeky-rabbit.ttf"
-#define FONT_SANS_SEMIBOLD "themes/base/fonts/cheeky-rabbit.ttf"
-#define FONT_SANS_BOLD "themes/base/fonts/cheeky-rabbit.ttf"
+#define FONT_SANS_REGULAR "themes/waifu/fonts/cheeky-rabbit.ttf"
+#define FONT_SANS_SEMIBOLD "themes/waifu/fonts/cheeky-rabbit.ttf"
+#define FONT_SANS_BOLD "themes/waifu/fonts/cheeky-rabbit.ttf"
