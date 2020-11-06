e92a0f5e58354d697209f539ef7c89e24ea1e8e8
Martin Lillepuu <martin@mlp.ee>
2020-11-06 22:13:33 +0200

    more merge fixes

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -64,7 +64,6 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01, 0.065, 0.2], [0.001, 0.015, 0.025]]
 
     if candidate == CAR.FORESTER_HYBRID:
-      ret.safetyParam = 2 # Different safety mode
       ret.mass = 1568. + STD_CARGO_KG
       ret.wheelbase = 2.67
       ret.centerToFront = ret.wheelbase * 0.5
