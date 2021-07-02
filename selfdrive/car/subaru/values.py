5ce68eec0360b7bfd2619b5a43f77e145eb21fe8
ClockeNessMnstr <locke.dftc@gmail.com>
2021-07-01 21:48:22 -0400

    Add 2019 Forester Sport FPv2

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -218,20 +218,25 @@ FW_VERSIONS = {
   },
   CAR.FORESTER: {
     # 2021 Subaru Forester - UDM / @gotham
+    # 2019 Subaru Forester - UDM / @clockenessmnstr
     # Ecu, addr, subaddr: ROM ID
     (Ecu.esp, 0x7b0, None): [
+      b'\xa3 \030\024\000',
       b'\xa3  \024\000',
     ],
     (Ecu.eps, 0x746, None): [
       b'\x8d\xc0\004\000',
     ],
     (Ecu.fwdCamera, 0x787, None): [
+      b'\000\000e!\037@ \021',
       b'\000\000e\x97\037@ 0',
     ],
     (Ecu.engine, 0x7e0, None): [
+      b'\xb6\"`A\a',
       b'\xcf"`0\a',
     ],
     (Ecu.transmission, 0x7e1, None): [
+      b'\032\xf6B0\000',
       b'\032\xf6F`\000',
     ],
   },
