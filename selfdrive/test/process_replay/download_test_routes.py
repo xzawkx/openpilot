4508275b43221c7c5d19effc499f2831bea48487
Martin Lillepuu <martin@mlp.ee>
2021-07-20 10:47:38 +0300

    update download_test_routes.py

--- selfdrive/test/process_replay/download_test_routes.py
+++ selfdrive/test/process_replay/download_test_routes.py
@@ -2,7 +2,7 @@
 import os
 import requests
 
-from selfdrive.test.process_replay.test_processes import segments, get_segment
+from selfdrive.test.process_replay.test_processes import segments
 
 BASE_URL = "https://commadataci.blob.core.windows.net/openpilotci/"
 
@@ -11,9 +11,7 @@ for car_brand, segment in segments:
   route_name, segment_num = segment.rsplit("--", 1)
   rlog_path = "%s/%s" % (route_name.replace("|", "/"), segment_num)
   rlog_fn = rlog_path + "/rlog.bz2"
-  #rlog_url = get_segment(segment)
-  route_name, segment_num = segment.rsplit("--", 1)
-  rlog_url = BASE_URL + "%s/%s/rlog.bz2" % (route_name.replace("|", "/"), segment_num)
+  rlog_url = BASE_URL + rlog_fn
 
   r = requests.get(rlog_url)
   if r.status_code == 200:
