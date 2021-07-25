d4681b23e910907ca3cb1d152431fb02fae43806
Martin Lillepuu <martin@mlp.ee>
2021-07-25 15:52:15 +0300

    Add FPv2: 2019 Outback - UDM

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -352,6 +352,7 @@ FW_VERSIONS = {
     # 2018 Subaru Outback 2.0d - ADM / @Richo
     # 2019 Outback 2.5i Premium / @Z-dawg Swizzlepants
     # 2019 Outback Touring 3.6R / @danyo
+    # 2019 Outback - UDM / @Steven C
     # Ecu, addr, subaddr: ROM ID
     (Ecu.esp, 0x7b0, None): [
       b'\x8b\x97\xac\x00',
@@ -360,6 +361,7 @@ FW_VERSIONS = {
       b'\x8b\x97\xbc\x00',
       b'\x8b\x99\xac\x00',
       b'\x9b\x9a\xac\000',
+      b'\x9b\x97\xbe\x10',
     ],
     (Ecu.eps, 0x746, None): [
       b'{\xb0\x00\x00',
@@ -371,6 +373,7 @@ FW_VERSIONS = {
       b'\x00\x00d\x95\x00\x00\x00\x00',
       b'\x00\x00d\x95\x1f@ \x0f',
       b'\x00\x00d\xfe\x00\x00\x00\x00',
+      b'\x00\x00e\x19\x1f@ \x15',
     ],
     (Ecu.engine, 0x7e0, None): [
       b'\xb5"@p\a',
@@ -380,6 +383,7 @@ FW_VERSIONS = {
       b'\xb5b@1\x07',
       b'\xb5q\xe0@\a',
       b'\xc4+@0\a',
+      b'\xc4b@p\a',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xbc\xf2@\x81\x00',
