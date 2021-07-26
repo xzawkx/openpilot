5ded4abc84c1c2629adca51bc9473dc88dad4255
Martin Lillepuu <martin@mlp.ee>
2021-07-26 23:04:56 +0300

    Add ubloxGnss to ui.c

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -308,7 +308,7 @@ QUIState::QUIState(QObject *parent) : QObject(parent) {
   ui_state.sm = std::make_unique<SubMaster, const std::initializer_list<const char *>>({
     "modelV2", "controlsState", "liveCalibration", "radarState", "deviceState", "roadCameraState",
     "pandaState", "carParams", "driverMonitoringState", "sensorEvents", "carState", "liveLocationKalman",
-    "gpsLocationExternal",
+    "gpsLocationExternal", "ubloxGnss",
   });
 
   ui_state.fb_w = vwp_w;
