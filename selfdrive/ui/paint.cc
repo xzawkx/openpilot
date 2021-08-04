d2ca5d9aced615cdb20824b815d964c24f893dfd
Martin Lillepuu <martin@mlp.ee>
2021-08-04 18:02:08 +0300

    Eng UI: replace viz_rect in paint.c

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -524,12 +524,12 @@ static void bb_ui_draw_UI(UIState *s)
 {
   //const UIScene *scene = &s->scene;
   const int bb_dml_w = 180;
-  const int bb_dml_x = (s->viz_rect.x + (bdr_is * 2));
-  const int bb_dml_y = (s->viz_rect.y + (bdr_is * 1.5)) + 220;
+  const int bb_dml_x = bdr_is * 2;
+  const int bb_dml_y = (bdr_is * 1.5) + 220;
 
   const int bb_dmr_w = 180;
-  const int bb_dmr_x = s->viz_rect.x + s->viz_rect.w - bb_dmr_w - (bdr_is * 2);
-  const int bb_dmr_y = (s->viz_rect.y + (bdr_is * 1.5)) + 220;
+  const int bb_dmr_x = s->fb_w - bb_dmr_w - (bdr_is * 2);
+  const int bb_dmr_y = (bdr_is * 1.5) + 220;
 
   bb_ui_draw_measures_left(s, bb_dml_x, bb_dml_y, bb_dml_w);
   bb_ui_draw_measures_right(s, bb_dmr_x, bb_dmr_y-20, bb_dmr_w);
