7889bc4e4871e8689598a808ec319516e9e7247c
Martin Lillepuu <martin@mlp.ee>
2021-08-14 18:06:53 +0300

    Add FPv2: 2020 Ascent - UDM / @ndtran

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -36,28 +36,34 @@ FW_VERSIONS = {
     # 2019 Ascent - UDM / @tvo
     # 2019 Ascent - UDM / @RCoaster
     # 2021 Ascent - UDM / @Sandy
+    # 2020 Ascent - UDM / @ndtran
     # Ecu, addr, subaddr: ROM ID
     (Ecu.esp, 0x7b0, None): [
       b'\xa5 \x19\x02\x00',
       b'\xa5 !\002\000',
+      b'\xf1\x82\xa5 \x19\x02\x00',
     ],
     (Ecu.eps, 0x746, None): [
       b'\x85\xc0\xd0\x00',
       b'\005\xc0\xd0\000',
+      b'\x95\xc0\xd0\x00',
     ],
     (Ecu.fwdCamera, 0x787, None): [
       b'\x00\x00d\xb9\x1f@ \x10',
       b'\000\000e~\037@ \'',
+      b'\x00\x00e@\x1f@ $',
     ],
     (Ecu.engine, 0x7e0, None): [
       b'\xbb,\xa0t\a',
       b'\xf1\x82\xbb,\xa0t\x87',
       b'\xf1\x82\xbb,\xa0t\a',
       b'\xf1\x82\xd9,\xa0@\a',
+      b'\xf1\x82\xd1,\xa0q\x07',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\x00\xfe\xf7\x00\x00',
       b'\001\xfe\xf9\000\000',
+      b'\x01\xfe\xf7\x00\x00',
     ],
   },
   CAR.IMPREZA: {
